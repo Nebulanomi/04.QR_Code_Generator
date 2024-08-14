924e0b9ce4d10cf39421ad2a436eb8eefd44ec24
Alexandre Pereira <alexandre.pereira@metyis.com>
2024-08-14 22:13:10 +0100

    added vscode code

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -23,11 +23,11 @@ terraform {
   }
 
   backend "azurerm" {
-    #resource_group_name  = var.resource_group_name
-    #storage_account_name = var.torage_account_name
-    #container_name       = var.container_name
-    #key                  = var.key
-  }
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "satsstoragealex"
+    container_name       = "tfstate"
+    key                  = "NiRMJ4RbraIVVEcbUW7R9aI1CpexJRl+YV9bouDZZzuu/LtGX3EgbHWdcbA9F1spSe16zTkWL2rW+AStnETCPw=="
+}
 }
 
 # Configure the Microsoft Azure Provider
@@ -35,8 +35,8 @@ provider "azurerm" {
   skip_provider_registration = false # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 
-  #  subscription_id = var.subscription_id
-  #  client_id       = var.client_id
-  #  client_secret   = var.client_secret
-  #  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
