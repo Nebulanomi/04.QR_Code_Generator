6c542711280ea8d9e5bbc797505beeb5e8c29561
Alexandre Pereira <144285453+Nebulanomi@users.noreply.github.com>
2024-08-14 09:43:54 +0100

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -23,10 +23,10 @@ terraform {
 }
 
   backend "azurerm" {
-      resource_group_name  = TF_VAR_resource_group_name
-      storage_account_name = TF_VAR_storage_account_name
-      container_name       = TF_VAR_container_name
-      key                  = TF_VAR_key
+      #resource_group_name  = TF_VAR_resource_group_name
+      #storage_account_name = TF_VAR_storage_account_name
+      #container_name       = TF_VAR_container_name
+      #key                  = TF_VAR_key
   }
 }
 
