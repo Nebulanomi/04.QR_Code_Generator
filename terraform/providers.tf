c1248fb36966a03301818efcee2a798ed9556c89
Alexandre Pereira <144285453+Nebulanomi@users.noreply.github.com>
2024-08-14 22:15:32 +0100

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -26,7 +26,7 @@ terraform {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "satsstoragealex"
     container_name       = "tfstate"
-    key                  = "NiRMJ4RbraIVVEcbUW7R9aI1CpexJRl+YV9bouDZZzuu/LtGX3EgbHWdcbA9F1spSe16zTkWL2rW+AStnETCPw=="
+    key                  = ""
 }
 }
 
