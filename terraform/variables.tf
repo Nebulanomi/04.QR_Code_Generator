c07f0784e69d8d106a310612f3dc47f3770eba71
Alexandre Pereira <alexandre.pereira@metyis.com>
2024-08-14 15:18:35 +0100

    New changes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -41,11 +41,13 @@ variable "tenant_id" {
 # RG variables
 variable "resource_group_location" {
   type        = string
+  default     = "North Europe"
   description = "Location of the resource group."
 }
 
 variable "resource_group_name_prefix" {
   type        = string
+  default     = "rg"
   description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
 }
 
